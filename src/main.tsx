d68ab1e252cda7aaf16b627e6f945af3490c6e2e
drmixer <130939838+drmixer@users.noreply.github.com>
2025-06-29 21:35:24 -0700

    Fix AuthProvider wrapping order

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,18 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext.tsx';
 import { GitHubProvider } from './hooks/useGitHub.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <GitHubProvider>
-        <App />
-      </GitHubProvider>
+      <AuthProvider>
+        <GitHubProvider>
+          <App />
+        </GitHubProvider>
+      </AuthProvider>
     </BrowserRouter>
   </StrictMode>
 );
