015043a8d5f594ce8fd3d44dde9c9be6389ab02a
Josh M <130939838+drmixer@users.noreply.github.com>
2025-07-21 20:34:49 -0700

    Update RecruiterDashboard.tsx

--- src/pages/RecruiterDashboard.tsx
+++ src/pages/RecruiterDashboard.tsx
@@ -6,7 +6,7 @@ import { Navigate } from 'react-router-dom';
 // ALL CUSTOM COMPONENT IMPORTS ARE NOW NAMED IMPORTS
 import { JobDetailView } from '../components/Jobs/JobDetailView';
 import { JobsDashboard } from '../components/Jobs/JobsDashboard';
-import { DeveloperDirectory } from '../components/DeveloperDirectory';
+import DeveloperDirectory from '../components/DeveloperDirectory';
 import { MessageList } from '../components/Messages/MessageList';
 import { MessageThread } from '../components/Messages/MessageThread';
 import { NotificationList } from '../components/Notifications/NotificationList';
