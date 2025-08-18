444f28eb65e28b833cdaa680133473c65538c55e
Josh M <130939838+drmixer@users.noreply.github.com>
2025-08-17 23:34:20 -0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext.tsx';
 import ScrollToTop from './components/ScrollToTop.tsx';
-import { HelmetProvider } from 'react-helmet-async';
+import { HelmetProvider, Helmet } from 'react-helmet-async';
 import { GitHubProvider } from './hooks/useGitHub.tsx'; // Ensured only one import
 import './index.css';
 
@@ -12,6 +12,10 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <HelmetProvider>
+        {/* Default site-wide title, overridden by pages that set their own */}
+        <Helmet>
+          <title>GitTalent - Connecting Devs and Recruiters</title>
+        </Helmet>
         <ScrollToTop />
         <AuthProvider>
           <App />
